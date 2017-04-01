a72618bd4764f7c684dcfcae5ca708c0d4f3dce1
Leo Generali <leogen17@gmail.com>
2017-03-31 20:52:23 -0400

    Make sass compile into one file

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,10 +2,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
+    gulp.src('sass/styles.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css/'));
 });
+
+
 
 gulp.task('default',function() {
     gulp.watch('sass/**/*.scss',['styles']);
