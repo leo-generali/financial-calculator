0e73d7220c5888274684552d7eefb8d1fbbfb2be
Leo Generali <leogen17@gmail.com>
2017-03-31 23:00:16 -0400

    Finalize card CSS

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('styles', function() {
-    gulp.src('sass/styles.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'));
+  gulp.src('sass/styles.scss')
+    .pipe(sourcemaps.init())
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer())
+    .pipe(sourcemaps.write('./css/maps'))
+    .pipe(gulp.dest('./css/'));
 });
 
-
-
 gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+  gulp.watch('sass/**/*.scss',['styles']);
 });
