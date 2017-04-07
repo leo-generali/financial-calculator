72440583fc84ffdbc947945bd6af910d003a66f4
Leo Generali <leogen17@gmail.com>
2017-04-06 20:56:07 -0400

    Get Chart.js working

--- js/script.js
+++ js/script.js
@@ -60,3 +60,32 @@ var compoundInterestPrincipal = function (balance, year, interestRate) {
 var futureValueSeries = function (contribution, year, interestRate) {
   return contribution * ((Math.pow(1 + interestRate, year) - 1) / interestRate); 
 }
+
+var createYearLabels = function(years) {
+  var currentYear = new Date().getFullYear();
+  var arr = [];
+  console.log(currentYear);
+  for(var i = 0; i < years.length; i++) {
+    arr.push(currentYear + i);
+  }
+
+  return arr;
+}
+
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+    datasets: [{
+      label: 'apples',
+      data: [12, 19, 3, 17, 6, 3, 7],
+      backgroundColor: "rgba(153,255,51,0.4)"
+    }, {
+      label: 'oranges',
+      data: [2, 29, 5, 5, 2, 3, 10],
+      backgroundColor: "rgba(255,153,0,0.4)"
+    }]
+  }
+});
